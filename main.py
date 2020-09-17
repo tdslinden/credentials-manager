3c211c309ccb07626123913e85b52cabb087cfe4
tdslinden <linden-k@hotmail.com>
2020-09-16 21:49:47 -0700

    Update the update function. Add the get credentials functionality.

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import config
 from mysql.connector import errorcode
 from datetime import date, datetime, timedelta
 
-
 commands = "\n" \
            "------------------------------\n" \
            "What would you like to do?\n" \
            "------------------------------\n" \
-           "q: Quit Program\n" \
            "i: Insert New Credentials\n" \
            "u: Update Current Credentials\n" \
-           "g: Generate Password\n" \
+           "g: Get Credentials\n" \
+           "p: Generate Password\n" \
            "d: Delete Credentials\n" \
+           "q: Quit Program\n" \
            "------------------------------\n" \
            ": "
 
@@ -37,8 +37,10 @@ def run_mysql():
             if command == "i":
                 insert_credentials(connector)
             elif command == "u":
-                insert_credentials(connector)
-            elif command == "gn":
+                run_update_prompt(connector)
+            elif command == "g":
+                get_credentials(connector)
+            elif command == "p":
                 generate_password()
             elif command == "d":
                 delete_credentials()
@@ -90,28 +92,44 @@ def insert_credentials(connector):
     cursor = connector.cursor()
     today = datetime.now().date()
 
-    insert_credentials_command = ("INSERT INTO credentials "
-                                  "(username, password, platform, created, lastUpdated) "
-                                  "VALUES (%s, %s, %s, %s, %s)")
-    
+    insert_credentials_query = ("INSERT INTO credentials "
+                                "(username, password, platform, created, lastUpdated) "
+                                "VALUES (%s, %s, %s, %s, %s)")
+
     username, password, platform = run_insert_prompt()
     credentials_data = (username, password, platform, today, today)
-    cursor.execute(insert_credentials_command, credentials_data)
+    cursor.execute(insert_credentials_query, credentials_data)
     connector.commit()
 
 
-def run_update_prompt():
-    command = "\n" \
-              "------------------------------\n" \
-              "What would you like to update?\n" \
-              "------------------------------\n" \
-              "u: Username\n" \
-              "p: Password\n" \
-              "------------------------------\n" \
-              ": "
+def run_update_prompt(connector):
+    username = input("\nEnter the username you would like to update: ")
+    platform = input("Enter the platform the username is associated to: ")
 
-    if command == "p":
-        password = input("Enter new password: ")
+    select_credentials_query = ("SELECT EXISTS (SELECT * FROM credentials where username = '{}' AND"
+                                " platform = '{}')").format(username, platform)
+
+    select_credentials_query1 = ("SELECT count(1) FROM credentials where username = '{}' AND"
+                                 " platform = '{}'").format(username, platform)
+
+    cursor = connector.cursor()
+    cursor.execute(select_credentials_query1)
+
+    for (count) in cursor:
+        print("{}".format(count))
+
+    # command = input("\n"
+    #                 "------------------------------\n"
+    #                 "What would you like to update?\n"
+    #                 "------------------------------\n"
+    #                 "u: Username\n"
+    #                 "p: Password\n"
+    #                 "b: Both Username and Password"
+    #                 "------------------------------\n"
+    #                 ": ")
+    #
+    # if command == "p":
+    #     password = input("Enter new password: ")
 
 
 def update_credentials(connector):
@@ -122,8 +140,39 @@ def generate_password():
     x = 4
 
 
-def get_credentials():
-    x = 2
+def run_get_prompt():
+    command = input("\n"
+                    "------------------------------\n"
+                    "What would you like to do?\n"
+                    "------------------------------\n"
+                    "1. Get all credentials.\n"
+                    "2. Get credentials based on platform.\n"
+                    "------------------------------\n"
+                    ":"
+                    )
+
+    if command == '2':
+        platform = input("\nEnter the platform: ")
+        return platform
+
+    return "all"
+
+
+def get_credentials(connector):
+    target_platform = run_get_prompt()
+    cursor = connector.cursor()
+
+    if target_platform == "all":
+        get_all_credentials_query = "SELECT username, password, platform FROM credentials"
+        cursor.execute(get_all_credentials_query)
+    else:
+        get_credentials_with_platform = "SELECT username, password, platform FROM credentials WHERE platform = '{}'".format(target_platform)
+        cursor.execute(get_credentials_with_platform)
+
+    print("\n--------------------------------------------------------------------------------\nCredentials")
+    for (username, password, platform) in cursor:
+        print("--------------------------------------------------------------------------------")
+        print("Username: {:<20} | Password: {:<15} | Platform: {:<10}".format(username, password, platform))
 
 
 def delete_credentials():
